ff35f5ca20cbcfd4550d8cbec7147f5121919b8b
NChijiokeGodwin <nchijiokegodwin@yahoo.com>
2021-07-24 23:20:25 -0400

    sucessful integration with PUT and POST api

--- frontend/gui/src/components/Form.js
+++ frontend/gui/src/components/Form.js
@@ -1,31 +1,52 @@
 import React from 'react';
+import axios from 'axios';
 import { Form, Input, Button } from 'antd';
 
-
 class CustomForm extends React.Component {
-  
-  formRef = React.createRef();
-    constructor(props) {
-       super(props);
-       this.state = { title: "", content: "" };
-    }    
-  
-    handleSubmit = (event) => {
-      event.preventDefault();
-      alert('Success')
+    formRef = React.createRef();
+
+    handleFormSubmit = (event, requestType, articleID) => {
+        const title = this.state.title;
+        const content = this.state.content;    
+        
+        switch ( requestType ) {
+            case 'post':
+                return axios.post('http://127.0.0.1:8000/api/', {
+                    title: title,
+                    content: content
+                })
+                .then(res => console.log(res))
+                .catch(error => console.err(error));
+            case 'put':
+                return axios.put(`http://127.0.0.1:8000/api/${articleID}/`, {
+                    title: title,
+                    content: content
+                })
+                .then(res => console.log(res))
+                .catch(error => console.err(error));
+        }
+        this.formRef.current.resetFields();
     }
 
+    
     handleChange = (event) => {
-      let nam = event.target.name;
-      let val = event.target.value;
-      this.setState({[nam]: val});
-      console.log(nam, val)
+        let nam = event.target.name;
+        let val = event.target.value;
+        this.setState({[nam]: val});
+        console.log(nam, val)
     }
-  
+
+
     render() {
-      return(
-          <div>
-            <Form ref={this.formRef} onSubmit={ this.handleSubmit }>
+        return (
+            <div>
+            <Form  onFinish={
+                (event) => this.handleFormSubmit(
+                    event, 
+                    this.props.requestType,
+                    this.props.articleID
+                )} 
+                ref={this.formRef}>
                 <Form.Item label="Title">
                 <Input name="title" placeholder="Put a title here" onChange={this.handleChange} />
                 </Form.Item>
@@ -36,10 +57,9 @@ class CustomForm extends React.Component {
                 <Button type="primary" htmlType="submit" >{this.props.btnText}</Button>
                 </Form.Item>
             </Form>
-          </div>
-        
-      );
+            </div>
+        );
     }
-  }
+};
 
-  export default CustomForm;
+export default CustomForm;
